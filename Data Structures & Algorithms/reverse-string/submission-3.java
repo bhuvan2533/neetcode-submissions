6c130ecbf1281b0d27d88c44f642500a4269f6e2
Bhuvan S <88939855+bhuvan2533@users.noreply.github.com>
2026-07-15 16:12:07 +0530

    Add: reverse-string - submission-3

---
+++ Data Structures & Algorithms/reverse-string/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length-1;
+
+        while(left<=right)
+            swap(left++, right--, s);
+    }
+
+    private void swap(int left, int right, char[]s) {
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+    }
+}
